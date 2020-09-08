c9056d5ad955903aa0cb529601ce34f5edf55188
Johan Pereira <2079700+jwpereira@users.noreply.github.com>
2020-09-08 17:25:19 -0400

    Update index.ts to drive core action

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 // Copyright 2020 The MathWorks, Inc.
 
 import * as core from "@actions/core";
+import * as exec from "@actions/exec";
 import * as matlab from "./matlab";
 
 export async function run() {
+    const platform = process.platform;
     const workspaceDir = process.cwd();
     const command = core.getInput("command");
 
-    await matlab.runCommand(workspaceDir, command);
+    const helperScript = await core.group("Generate script", async () => {
+        const helperScript = await matlab.generateScript(workspaceDir, command);
+        core.info("Sucessfully generated script");
+        return helperScript;
+    });
+
+    await core.group("Run command", async () => {
+        await matlab.runCommand(helperScript, platform, exec.exec);
+    });
 }
 
 run().catch((e) => {
