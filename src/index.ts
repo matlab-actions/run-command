ab4fec5d22bbab804c3a61961de0e58c6810c153
Mark Cafaro <mcafaro@mathworks.com>
2025-09-04 09:21:11 -0400

    Update comment

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function run() {
 
     const helperScript = await matlab.generateScript(workspaceDir, command);
     const execOpts = {
-        env: {...process.env, MW_BATCH_LICENSING_ONLINE:'true'}  // Disabled while we work out online licensing kinks
+        env: {...process.env, MW_BATCH_LICENSING_ONLINE:'true'}  // Remove when online batch licensing is the default
     };
     await matlab.runCommand(helperScript, platform, architecture, (cmd,args)=>exec.exec(cmd,args,execOpts), startupOpts);
 }
