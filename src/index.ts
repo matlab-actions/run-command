748e5ac03ba9d9b86003af1f7c12ade579759781
Kapil Gupta <kapilg@mathworks.com>
2025-12-02 11:15:25 +0530

    use new test summary api

--- src/index.ts
+++ src/index.ts
@@ -33,10 +33,7 @@ async function run() {
 
         buildSummary.processAndDisplayBuildSummary(runnerTemp, runId, actionName);
 
-        const testResultsData = testResultsSummary.getTestResults(runnerTemp, runId, workspaceDir);
-        if(testResultsData) {
-            testResultsSummary.writeSummary(testResultsData, actionName);
-        }
+        testResultsSummary.processAndDisplayTestSummary(runnerTemp, runId, actionName, workspaceDir);
     });
 }
 
