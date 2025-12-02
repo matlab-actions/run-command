62c7d87d8b1d54900ed5169278e196db5de350d9
Kapil Gupta <kapilg@mathworks.com>
2025-12-02 18:49:29 +0530

    revert plugins path logic

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 // TODO: update common-utils version when new version is released
 import { matlab, testResultsSummary, buildSummary } from "common-utils";
+import * as path from "path";
 
 /**
  * Gather action inputs and then run action.
@@ -13,7 +14,8 @@ async function run() {
     const architecture = process.arch;
     const workspaceDir = process.cwd();
 
-    const command = core.getInput("command");
+    const pluginsPath = path.join(__dirname, "plugins").replaceAll("'","''");
+    const command = "addpath('"+ pluginsPath +"'); " + core.getInput("command");
     const startupOpts = core.getInput("startup-options").split(" ");
 
     const helperScript = await matlab.generateScript(workspaceDir, command);
