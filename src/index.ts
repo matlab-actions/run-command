a474a8d822192978c4e0b08972a9c9cf65b130dc
David Buzinski <103441853+davidbuzinski@users.noreply.github.com>
2026-04-07 10:44:55 -0400

    Format and update actions (#93)
    * Add format check and update CI to use setup-matlab@v3
    
    * update contributing
    
    * use latest release of common-utils

--- src/index.ts
+++ src/index.ts
@@ -19,19 +19,32 @@ async function run() {
     const execOpts = {
         env: {
             ...process.env,
-            MW_BATCH_LICENSING_ONLINE:'true', // Remove when online batch licensing is the default
-            "MW_MATLAB_BUILDTOOL_DEFAULT_PLUGINS_FCN_OVERRIDE": "buildframework.getDefaultPlugins",
-        }
+            MW_BATCH_LICENSING_ONLINE: "true", // Remove when online batch licensing is the default
+            MW_MATLAB_BUILDTOOL_DEFAULT_PLUGINS_FCN_OVERRIDE: "buildframework.getDefaultPlugins",
+        },
     };
-    await matlab.runCommand(helperScript, platform, architecture, (cmd: string, args?: string[])=>exec.exec(cmd,args,execOpts), startupOpts).finally(() => {
-        const runnerTemp = process.env.RUNNER_TEMP || '';
-        const runId = process.env.GITHUB_RUN_ID || '';
-        const actionName = process.env.GITHUB_ACTION || '';
+    await matlab
+        .runCommand(
+            helperScript,
+            platform,
+            architecture,
+            (cmd: string, args?: string[]) => exec.exec(cmd, args, execOpts),
+            startupOpts,
+        )
+        .finally(() => {
+            const runnerTemp = process.env.RUNNER_TEMP || "";
+            const runId = process.env.GITHUB_RUN_ID || "";
+            const actionName = process.env.GITHUB_ACTION || "";
 
-        buildSummary.processAndAddBuildSummary(runnerTemp, runId, actionName);
-        testResultsSummary.processAndAddTestSummary(runnerTemp, runId, actionName, workspaceDir);
-        core.summary.write();
-    });
+            buildSummary.processAndAddBuildSummary(runnerTemp, runId, actionName);
+            testResultsSummary.processAndAddTestSummary(
+                runnerTemp,
+                runId,
+                actionName,
+                workspaceDir,
+            );
+            core.summary.write();
+        });
 }
 
 run().catch((e) => {
